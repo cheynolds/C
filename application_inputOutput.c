27a3002428d726a1dba5ee069bf52803a3cb3ba4
Chris <github@cheynolds.com>
2022-07-04 11:50:07 -0700

    Update application_inputOutput.c

--- application_inputOutput.c
+++ application_inputOutput.c
@@ -23,3 +23,40 @@ The quotient is 3.
 
 */
 
+#include <stdio.h>
+
+int main()
+{
+    int userInput_one = 0;
+    int userInput_two = 0;
+    int numberSum = 0;
+    int numberDifference = 0;
+    int numberProduct = 0;
+    int numberQuotient = 0;
+    
+    char userName[] = "YourName";
+    char classNumber[] = "ClassNumber";
+    
+    printf("My name is %s, I'm in course %s. I've taken: \n", userName,classNumber);
+    printf("Python programming \n");
+    printf("C++ programming \n\n");
+    printf("Give me a number: ");
+    scanf("%d", &userInput_one);
+    printf("Give me another number: ");
+    scanf("%d", &userInput_two);
+    printf("\n");
+    
+    numberSum = userInput_one + userInput_two;
+    numberDifference = userInput_one - userInput_two;
+    numberProduct = userInput_one * userInput_two;
+    numberQuotient = userInput_one / userInput_two;
+    
+    printf("The sum is %d.\n", numberSum);
+    printf("The difference is %d.\n", numberDifference);
+    printf("The product is %d.\n", numberProduct);
+    printf("The quotient is %d.\n", numberQuotient);
+
+    
+    return 0;
+    
+}
