a4d553d35556600814c3ebdb075a8fd938eb9791
Chris <github@cheynolds.com>
2022-07-04 11:04:38 -0700

    add problem statement

--- example_doWhile.c
+++ example_doWhile.c
@@ -1,1 +1,10 @@
 
+/*
+Define two integer variables.
+Ask the user for a value to load into the first variable.
+Inside a do-while loop.
+Print out the first value.
+Ask for another value.
+Loop again if the second value is larger than the first.
+*/
+
