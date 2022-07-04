41c6a0635ce5822259fd7d1841663614af100adc
Chris <github@cheynolds.com>
2022-07-04 11:26:16 -0700

    Create application_inputOutput.c

---
+++ application_inputOutput.c
@@ -0,0 +1,25 @@
+/*
+Program Description
+Write a program that prints out your name, the course ID of this class, what programming/computer courses you've taken.
+Ask the user for two numbers.
+Load the user numbers into variables.
+Show the sum of the two numbers, the difference, the product and the quotient.
+For the difference subtract the second number from the first, for the quotient, use the first number as the numerator(dividend) and the second as the denominator(divisor).
+ 
+
+Sample Output:
+
+My name is Glenn, I'm in course 123456. I've taken:
+C Programming
+C++ Programming
+
+Give me a number: 7
+Give me a second number: 2
+
+The sum is 9.
+The difference is 5.
+The product is 14.
+The quotient is 3.
+
+*/
+
