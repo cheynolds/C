b93cfd70bb27e6e1e039fc493955face9c2934bf
Chris <github@cheynolds.com>
2022-07-12 17:12:33 -0700

    Update application_payroll_loop.c
    outline of application added

--- application_payroll_loop.c
+++ application_payroll_loop.c
@@ -1,1 +1,62 @@
-  int
+/*
+Payroll 2.0
+
+Update the first program.
+
+Use for loops to process the data for 5 employees.
+One loop to load the data
+One loop to print the output. 
+In for loops, if any of the entered fields are –1, break out of the loop immediately after getting -1
+Update the output as shown in the sample data.
+Use arrays to store the user input. 
+ 
+
+The program logic will first load all of the data, until the user enters the max number of records, or they input -1 for one of the fields. After the data is loaded, it will then be processed and output generated.
+
+Example 
+
+Enter name: Glenn
+
+Enter hourly rate: 2.00
+
+Enter hours worked: 50
+
+ 
+
+Enter name: Stephanie
+
+Enter hourly rate: 25.00
+
+Enter hours worked: 50
+
+ 
+
+Enter name: -1
+
+ 
+
+Pay to: Glenn
+
+Hours worked:                50.0
+
+Hourly rate:                 $    2.00
+
+Gross pay:                  $110.00
+
+Base pay:                   $  80.00
+
+Overtime pay:             $  30.00
+
+Taxes paid:                 $  22.00
+
+Net pay:                      $  88.00
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Payroll 2.0");
+
+    return 0;
+}
